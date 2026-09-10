0c8573cc6232682c9c49e3d2df84a392426b3d31
fggghub-ux <medieval.snow0@gmail.com>
2026-09-10 09:48:49 +0800

    Update loves.js

--- js/loves.js
+++ js/loves.js
@@ -2590,8 +2590,8 @@ ${chatContext ? `【近期 iMessage 上下文】\n${chatContext}\n\n` : ''}要�
             container.innerHTML = `
                 <div class="loves-placeholder">
                     <i class="fas fa-heart-crack"></i>
-                    <p>Then I must have drowned long ago.
-                    Because if your eyes are an ocean,
+                    <p>Then I must have drowned long ago.<br><br>
+                    Because if your eyes are an ocean,<br>
                     I was born to sink.</p>
                 </div>
             `;
