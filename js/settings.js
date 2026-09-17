3d26ef0680ddf844ae3d318293b01a53f81304d1
fggghub-ux <medieval.snow0@gmail.com>
2026-09-17 12:53:12 +0800

    Migrate legacy Lover icon path on iPad

--- js/settings.js
+++ js/settings.js
@@ -166,6 +166,12 @@
         'dock-icon-youtube': APP_ICON_BASE + 'youtube.jpg'
     };
 
+    const LEGACY_LOVER_ICON_FILE = ['love', 's.jpg'].join('');
+    const isLegacyLoverIcon = (icon) => {
+        const cleanPath = String(icon || '').split(/[?#]/, 1)[0];
+        return cleanPath === LEGACY_LOVER_ICON_FILE || cleanPath.endsWith('/' + LEGACY_LOVER_ICON_FILE);
+    };
+
     let themeState = {
         bgUrl: null,
         uiChineseEnabled: false,
@@ -274,7 +280,11 @@
                     savedThemeState.apps.forEach(savedApp => {
                         const existingApp = themeState.apps.find(a => a.id === savedApp.id);
                         if (existingApp) {
-                            if (savedApp.icon) existingApp.icon = savedApp.icon; // 空值不覆盖默认图
+                            if (savedApp.id === 'app-icon-8' && isLegacyLoverIcon(savedApp.icon)) {
+                                existingApp.icon = DEFAULT_APP_ICONS['app-icon-8'];
+                            } else if (savedApp.icon) {
+                                existingApp.icon = savedApp.icon; // 空值不覆盖默认图
+                            }
                             if (savedApp.id === 'app-icon-6') {
                                 existingApp.name = 'Library';
                             } else if (savedApp.id === 'app-icon-8' && ['Diary', 'Lover', ['Love', 's'].join('')].includes(savedApp.name)) {
